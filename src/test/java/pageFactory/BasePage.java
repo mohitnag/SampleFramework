32477a2b0c8a6472b071410402844f4b3695d3fb
mohitnag <mohitnag@gmail.com>
2019-10-25 15:55:41 +1100

    make it work with latest java

--- src/test/java/pageFactory/BasePage.java
+++ src/test/java/pageFactory/BasePage.java
@@ -3,11 +3,8 @@ package pageFactory;
 import config.LoadConfiguration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 public class BasePage {
     protected WebDriver driver;
